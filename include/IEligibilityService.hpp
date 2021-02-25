dc7e90f3d4e9bcdfd7e49768818091f5c7be2670
Andris Strals <andris.strals@gmail.com>
2021-02-25 23:20:54 +0000

    EligibilityService interface defined

---
+++ include/IEligibilityService.hpp
@@ -0,0 +1,19 @@
+#ifndef IELIGIBILITYSERVICE_HPP
+#define IELIGIBILITYSERVICE_HPP
+
+template <typename T>
+class IEligibilityService {
+public:
+    enum class Eligibility {
+        CUSTOMER_ELIGIBLE,
+        CUSTOMER_INELIGIBLE
+    };
+
+    virtual ~IEligibilityService() = default;
+
+    // checks if provided account is eligible for rewards
+    // throws exception on technical fault or invalid account number
+    virtual IEligibilityService::Eligibility checkEligibility(const T &accountNo) = 0;
+};
+
+#endif      //IELIGIBILITYSERVICE_HPP
